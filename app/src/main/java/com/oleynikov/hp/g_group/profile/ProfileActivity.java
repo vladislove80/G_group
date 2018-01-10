4b0a6b9d37afa4e8b2fc817c62c47892189baadb
vladislavu-hyuna <vladislav.ulyanickiy@playster.com>
2018-01-10 14:58:54 +0200

    ProfileActivity small refactoring

--- app/src/main/java/com/oleynikov/hp/g_group/profile/ProfileActivity.java
+++ app/src/main/java/com/oleynikov/hp/g_group/profile/ProfileActivity.java
@@ -24,108 +24,103 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
-    RecyclerView mRecyclerViewCard ;
-    RecyclerView mRecyclerViewTransaction;
-    List<Balance> mBalance;
-    List<Transaction> mTransaction;
-    RecyclerView.LayoutManager mRecyclerViewLayoutManager;
-    RecyclerView.LayoutManager mRecyclerViewLayoutManagerTransaction;
-    RecyclerViewBalanceCardAdapter mRecyclerViewHorizontalAdapter;
-    RecyclerViewTransactionAdapter mRecyclerViewTransactionAdapter;
-    LinearLayoutManager mHorizontalLayout;
-    LinearLayoutManager mHorizontalLayoutTransaction;
-    ImageButton mImageButtonEditProfile;
-    ImageButton mImageButtonOpenSort;
-    ImageButton mImageButtonHome;
-    LinearLayout mLinearLayoutSort;
-    LinearLayout mLinearLayoutRVCard;
-    RelativeLayout mRelativeLayoutTimeTransaction;
-    Button mButtonOnDate;
-    Button mButtonOnCost;
-    ImageView mImageViewRotation;
 
+    private RecyclerView mRecyclerViewCards;
+    private RecyclerView mRecyclerViewTransactions;
+    private List<Balance> mBalance;
+    private List<Transaction> mTransaction;
+    private RecyclerView.LayoutManager mRecyclerViewLayoutManager;
+    private RecyclerView.LayoutManager mRecyclerViewLayoutManagerTransactions;
+    private RecyclerViewBalanceCardAdapter mRecyclerViewAdapterCards;
+    private RecyclerViewTransactionAdapter mRecyclerViewTransactionsAdapter;
+    private LinearLayoutManager mHorizontalLayoutCards;
+    private LinearLayoutManager mHorizontalLayoutTransactions;
+    private ImageButton mImageButtonEditProfile;
+    private ImageButton mImageButtonOpenSort;
+    private ImageButton mImageButtonHome;
+    private LinearLayout mLinearLayoutSort;
+    private LinearLayout mLinearLayoutRVCard;
+    private RelativeLayout mRelativeLayoutTimeTransaction;
+    private Button mButtonOnDate;
+    private Button mButtonOnCost;
+    private ImageView mImageViewRotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mRecyclerViewCard = (RecyclerView)findViewById(R.id.recyclerViewProfile);
-        mRecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mRecyclerViewCard.setLayoutManager(mRecyclerViewLayoutManager);
-        
-        mRecyclerViewTransaction = (RecyclerView)findViewById(R.id.recyclerTransaction);
-        mRecyclerViewLayoutManagerTransaction = new LinearLayoutManager(getApplicationContext());
-        mRecyclerViewTransaction.setLayoutManager(mRecyclerViewLayoutManagerTransaction);
-
-
-
-
+        setViews();
+        setListenersToViews();
         // Adding items to RecyclerView.
         addBalanceCardToRecyclerViewArrayList();
         addListTransactionToRecyclerViewArrayList();
 
-        mRecyclerViewHorizontalAdapter = new RecyclerViewBalanceCardAdapter(mBalance);
-        mHorizontalLayout = new LinearLayoutManager(ProfileActivity.this, LinearLayoutManager.HORIZONTAL, false);
-        mRecyclerViewCard.setLayoutManager(mHorizontalLayout);
-        mRecyclerViewCard.setAdapter(mRecyclerViewHorizontalAdapter);
+        setCardsRecycler();
+        setTransactionsRecycler();
+    }
 
-        mRecyclerViewTransactionAdapter = new RecyclerViewTransactionAdapter(mTransaction);
-        mHorizontalLayoutTransaction = new LinearLayoutManager(ProfileActivity.this,LinearLayoutManager.VERTICAL, false);
-        mRecyclerViewTransaction.setLayoutManager(mHorizontalLayoutTransaction);
-        mRecyclerViewTransaction.setAdapter(mRecyclerViewTransactionAdapter);
+    private void setTransactionsRecycler() {
+        mRecyclerViewTransactions = (RecyclerView) findViewById(R.id.recyclerTransactions);
+        mRecyclerViewLayoutManagerTransactions = new LinearLayoutManager(getApplicationContext());
+        mRecyclerViewTransactions.setLayoutManager(mRecyclerViewLayoutManagerTransactions);
+        mRecyclerViewTransactionsAdapter = new RecyclerViewTransactionAdapter(mTransaction);
+        mHorizontalLayoutTransactions = new LinearLayoutManager(ProfileActivity.this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerViewTransactions.setLayoutManager(mHorizontalLayoutTransactions);
+        mRecyclerViewTransactions.setAdapter(mRecyclerViewTransactionsAdapter);
+    }
 
-        mImageButtonHome = (ImageButton) findViewById(R.id.imageButtonHome);
+    private void setCardsRecycler() {
+        mRecyclerViewCards = (RecyclerView) findViewById(R.id.recyclerViewCards);
+        mRecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerViewCards.setLayoutManager(mRecyclerViewLayoutManager);
+        mRecyclerViewAdapterCards = new RecyclerViewBalanceCardAdapter(mBalance);
+        mHorizontalLayoutCards = new LinearLayoutManager(ProfileActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerViewCards.setLayoutManager(mHorizontalLayoutCards);
+        mRecyclerViewCards.setAdapter(mRecyclerViewAdapterCards);
+    }
+
+    private void setListenersToViews() {
         mImageButtonHome.setOnClickListener(this);
-        mImageButtonOpenSort = (ImageButton) findViewById(R.id.imageButtonOpenSort);
         mImageButtonOpenSort.setOnClickListener(this);
-        mImageButtonEditProfile = (ImageButton) findViewById(R.id.imageButtonEditProfile);
         mImageButtonEditProfile.setOnClickListener(this);
+        mButtonOnCost.setOnClickListener(this);
+        mButtonOnDate.setOnClickListener(this);
+    }
+
+    private void setViews() {
+        mImageButtonHome = (ImageButton) findViewById(R.id.imageButtonHome);
+        mImageButtonOpenSort = (ImageButton) findViewById(R.id.imageButtonOpenSort);
+        mImageButtonEditProfile = (ImageButton) findViewById(R.id.imageButtonEditProfile);
         mLinearLayoutSort = (LinearLayout) findViewById(R.id.LinearLayoutOpen);
         mLinearLayoutRVCard = (LinearLayout) findViewById(R.id.layoutRVCard);
         mRelativeLayoutTimeTransaction = (RelativeLayout) findViewById(R.id.layoutTimeTransaction);
-
         mButtonOnCost = (Button) findViewById(R.id.buttonOnCost);
-        mButtonOnCost.setOnClickListener(this);
         mButtonOnDate = (Button) findViewById(R.id.buttonOnDate);
-        mButtonOnDate.setOnClickListener(this);
         mImageViewRotation = (ImageView) findViewById(R.id.imageViewRotate);
         mLinearLayoutSort.setVisibility(View.GONE);
         mLinearLayoutRVCard.setVisibility(View.GONE);
         mRelativeLayoutTimeTransaction.setVisibility(View.GONE);
-
-
-
-
-
     }
-
 
     @Override
     public void onClick(View view) {
-
-        switch( view.getId()) {
+        switch (view.getId()) {
             case R.id.imageButtonOpenSort:
-
-
-
-                if ((mLinearLayoutSort.getVisibility() == View.VISIBLE))
-                {
+                if ((mLinearLayoutSort.getVisibility() == View.VISIBLE)) {
 //                    mLinearLayoutSort.setAlpha(0.0f);
-
                     mLinearLayoutSort.animate()
-                        .translationY(0)
-                        .alpha(0.0f)
-                        .setInterpolator(new DecelerateInterpolator())
-                        .setListener(new AnimatorListenerAdapter() {
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                super.onAnimationEnd(animation);
-                                mLinearLayoutSort.setVisibility(View.GONE);
-
-                                mLinearLayoutRVCard.setVisibility(View.GONE);
-                            }
-                        });
+                            .translationY(0)
+                            .alpha(0.0f)
+                            .setInterpolator(new DecelerateInterpolator())
+                            .setListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    mLinearLayoutSort.setVisibility(View.GONE);
+                                    mLinearLayoutRVCard.setVisibility(View.GONE);
+                                }
+                            });
                     mImageViewRotation.setRotation(180);
                     mRelativeLayoutTimeTransaction.animate()
                             .translationY(0)
@@ -135,17 +130,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                                 @Override
                                 public void onAnimationEnd(Animator animation) {
                                     super.onAnimationEnd(animation);
-
                                     mRelativeLayoutTimeTransaction.setVisibility(View.GONE);
-
                                 }
                             });
-
-
-                }
-                else
-                {
-
+                } else {
                     mLinearLayoutSort.setAlpha(0.0f);
                     mLinearLayoutSort.animate()
                             .translationY(0)
@@ -156,9 +144,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                                 public void onAnimationStart(Animator animation) {
                                     super.onAnimationStart(animation);
                                     mLinearLayoutSort.setVisibility(View.VISIBLE);
-
-
-
                                 }
                             });
                     mRelativeLayoutTimeTransaction.animate()
@@ -174,40 +159,31 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                             });
                     mImageViewRotation.setRotation(0);
                 }
-
-
                 break;
             case R.id.imageButtonHome:
-              onBackPressed();
+                onBackPressed();
                 break;
             case R.id.imageButtonEditProfile:
                 startActivity(new Intent(ProfileActivity.this, EditProfileActivity.class));
                 finish();
                 break;
             case R.id.buttonOnDate:
-
                 mButtonOnCost.setBackgroundColor(Color.BLACK);
                 mButtonOnCost.setTextColor(Color.WHITE);
                 mButtonOnDate.setBackgroundResource(R.drawable.rounded_button);
                 mButtonOnDate.setTextColor(Color.BLACK);
                 mLinearLayoutRVCard.setVisibility(View.GONE);
                 mRelativeLayoutTimeTransaction.setVisibility(View.VISIBLE);
-
-
                 break;
             case R.id.buttonOnCost:
-
                 mButtonOnDate.setBackgroundColor(Color.BLACK);
                 mButtonOnDate.setTextColor(Color.WHITE);
                 mButtonOnCost.setBackgroundResource(R.drawable.rounded_button);
                 mButtonOnCost.setTextColor(Color.BLACK);
                 mLinearLayoutRVCard.setVisibility(View.VISIBLE);
                 mRelativeLayoutTimeTransaction.setVisibility(View.GONE);
-               
-
                 break;
         }
-
     }
 
     public class Balance {
@@ -221,30 +197,23 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     // function to add items in RecyclerView.
-    public void addBalanceCardToRecyclerViewArrayList(){
-
+    public void addBalanceCardToRecyclerViewArrayList() {
         mBalance = new ArrayList<>();
-        mBalance.add(new Balance ("ONE","Normal"));
-        mBalance.add(new Balance ("ONE","Normal"));
-        mBalance.add(new Balance("ONE","Normal"));
-        mBalance.add(new Balance ("ONE","Normal"));
-        mBalance.add(new Balance ("ONE","Normal"));
-
-
+        mBalance.add(new Balance("ONE", "Normal"));
+        mBalance.add(new Balance("ONE", "Normal"));
+        mBalance.add(new Balance("ONE", "Normal"));
+        mBalance.add(new Balance("ONE", "Normal"));
+        mBalance.add(new Balance("ONE", "Normal"));
     }
 
     private void addListTransactionToRecyclerViewArrayList() {
         mTransaction = new ArrayList<>();
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-        mTransaction.add(new Transaction("22.01.22","00:00:00","34.50"));
-
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
+        mTransaction.add(new Transaction("22.01.22", "00:00:00", "34.50"));
     }
-
-
-
 }
